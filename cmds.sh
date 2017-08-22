15208e440eabcd7b6eef7bb773f156e2b5e51509
cesar_vldz <cesar.valdez@feedhenry.com>
2017-08-22 11:06:17 +0100

    adding security profile for docker

---
+++ cmds.sh
@@ -0,0 +1,17 @@
+function toby_stop {
+  ID=$(docker inspect --format="{{.Id}}" $1)
+  docker stop $1
+  docker rm $ID
+}
+
+
+function toby_iso {
+    docker run -it -d -p 8080:8080 -v $(pwd):/usr/src/app --security-opt seccomp=$2 --name $1 bench
+}
+
+
+Running
+```sh
+  docker run -i -d -p 8080:8080 -v \
+     $(pwd):/usr/src/app --name test bench
+```
--- cpu_bound/README.md
+++ cpu_bound/README.md
@@ -8,5 +8,7 @@ Building
 
 Running
 ```sh
-  docker run -i -d -p 8080:8080 -v $(pwd):/usr/src/app --name test bench
+  docker run -i -d -p 8080:8080 -v \
+   --security-opt seccomp=https://github.com/moby/moby/blob/master/profiles/seccomp/default.json \
+   $(pwd):/usr/src/app --name test bench
 ```
